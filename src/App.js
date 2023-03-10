dda70291231dec193b137053c062ecac2701f08a
subir-raju <subir.raju0@gmail.com>
2023-03-11 00:53:39 +0600

    final touch before hostingt

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import NavComponent from './components/NavComponent';
 import Section from './components/Section';
 import Footer from './components/Footer';
-//import Technologies from './components/Technologies';
+
 
 function App() {
   return (
--- src/components/Technologies.js
+++ src/components/Technologies.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './Technologies.css';
+import intrack from './assets/Intrack.png';
 import docket from './assets/Docket.png';
+import airticket from './assets/Airticket.png';
 
 const Technologies = () => {
     return(
@@ -18,12 +20,26 @@ const Technologies = () => {
                            <p> Utilized - <b>React.js, Microsrvice Architecture, MySQL</b></p>
                 </div>
                 <div className='description'>
+                    <img src={intrack} alt='' />
+                </div>
+            </div>
+            <div className='projects'>
+                <div className='description'>
+                    <h3>Docket</h3>
+                    <p>Docket is a website where users can keep track of 
+                        any movies, tv-series, anime, game, books instead of using 
+                        multiple sites for the same purpose. It also has features
+                        like giving own rating and posting blog about them.
+                         </p><br />
+                           <p> Utilized - <b>HTML, CSS, JavaScript, jQuery, PHP MySQL</b></p>
+                </div>
+                <div className='description'>
                     <img src={docket} alt='' />
                 </div>
             </div>
             <div className='projects'>
                 <div className='description'>
-                    <h3>InTrack</h3>
+                    <h3>Air Ticket Manager</h3>
                     <p>In the Senior Design course of my undergraduate final year,
                          built InTrack, which is a software tool to track RMG manufacturing
                           defects using real-time production data. Simple data from the 
@@ -32,21 +48,7 @@ const Technologies = () => {
                            <p> Utilized - <b>React.js, Microsrvice Architecture, MySQL</b></p>
                 </div>
                 <div className='description'>
-                    <img src={docket} alt='' />
-                </div>
-            </div>
-            <div className='projects'>
-                <div className='description'>
-                    <h3>InTrack</h3>
-                    <p>In the Senior Design course of my undergraduate final year,
-                         built InTrack, which is a software tool to track RMG manufacturing
-                          defects using real-time production data. Simple data from the 
-                          quality control can provide us with enhanced data-visuals into
-                           the overall production line. </p><br />
-                           <p> Utilized - <b>React.js, Microsrvice Architecture, MySQL</b></p>
-                </div>
-                <div className='description'>
-                    <img src={docket} alt='' />
+                    <img src={airticket} alt='' />
                 </div>
             </div>
             </div>
